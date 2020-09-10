3238f54b4c6d1602eba69db267b330597d192663
samuelmarina <samuellmarina@gmail.com>
2020-09-10 19:19:38 -0400

    Create loader

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
+import AppText from "./app/components/AppText";
+import MainScreen from "./app/screens/MainScreen";
+import LanguageTexts from "./app/components/LanguageTexts";
+
 export default function App() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <MainScreen />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  camera: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "black",
     justifyContent: "center",
   },
 });
